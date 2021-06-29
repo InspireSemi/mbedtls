8c1cf078cb128af492157b9cac29d7f34852b94f
Marc Karasek <mkarasek@inspiresemi.com>
2021-06-29 16:03:21 -0400

    Updating cluster address for sha2 module

--- include/hondo_sha256.h
+++ include/hondo_sha256.h
@@ -10,6 +10,28 @@
 #define _AC(X,Y)        (X##Y)
 #endif /* !__ASSEMBLER__*/
 
+#define     ADDR(hb, clx, cly, crx, cry, offset) ( (hb<<29)|(clx<<25)|(cly<<21)|(crx<<18)|(cry<<15)|offset )
+// Boot/Corner Cluster
+#define  CLUSTER0_X     0
+#define  CLUSTER0_Y     0
+//Corner Core
+#define  CLUSTER9_X     0
+#define  CLUSTER9_Y     9
+//Corner Core
+#define  CLUSTER70_X     7
+#define  CLUSTER70_Y     0
+//Corner Core
+#define  CLUSTER79_X     7
+#define  CLUSTER79_Y     9
+// Core 0
+#define  CLUSTER0_X     0
+#define  CLUSTER0_Y     0
+// SHA2 Mapping
+#define SHA2_X          0
+#define SHA2_Y          0
+
+#define  HB_1           1
+
 #define SHA256_FLAG_INITMIDSTATE 0b00000001
 #define SHA256_FLAG_INIT         0b00000010
 #define SHA256_FLAG_START        0b00000100
@@ -21,10 +43,11 @@
 #define SHA256_OSTATE 128
 #define SHA256_STATUS 160
 
-
 #define SHA256_FLAG_RUNNING      0b00000001
 #define SHA256_FLAG_DONE         0b00000010
 
-// SHA256 unit - SE corner
-#define SHA2560_CTRL_ADDR _AC(0x2EE00600,UL)
+// SHA256 unit - Corner 7,9
+#define SHA2560_OFFSET 0x600
+#define SHA2560_CTRL_ADDR ADDR(HB_1,CLUSTER79_X,CLUSTER79_Y,SHA2_X,SHA2_X,SHA2560_OFFSET)
+#define SHA2560_PTR ((void * const)SHA2560_CTRL_ADDR)
 #define SHA2560_CTRL_SIZE _AC(0x100,UL)
